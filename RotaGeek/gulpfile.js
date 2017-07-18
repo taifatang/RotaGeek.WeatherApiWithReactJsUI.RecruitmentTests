9304239f31dd8bbb1dcabe2976d341483945ea95
taifatang <taifatang@hotmail.com>
2017-07-19 00:31:36 +0100

    #ROTA-1 - Create UI and front end infrastructure

--- RotaGeek/gulpfile.js
+++ RotaGeek/gulpfile.js
@@ -1,7 +1,8 @@
 ﻿var gulp = require("gulp"),
     fs = require("fs"),
     less = require("gulp-less"),
-    sass = require("gulp-sass");
+    sass = require("gulp-sass"),
+    rename = require("gulp-rename");
 
 var paths = {
     webroot: "./wwwroot/"
@@ -11,7 +12,14 @@ paths.scss = paths.webroot + "css/**/*.scss";
 
 gulp.task("sass", function () {
     return gulp.src(paths.scss)
-        .pipe(sass())
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('wwwroot/css'));
+});
+
+gulp.task("sass-min", function () {
+    return gulp.src(paths.scss)
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(rename("site.min.css"))
         .pipe(gulp.dest('wwwroot/css'));
 });
 
